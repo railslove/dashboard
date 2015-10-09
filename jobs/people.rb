a77622caeb5b1a26b1ad853736c12089ecb85902
Stephan Pavlovic <stephan@railslove.com>
2015-10-09 17:18:50 +0200

    add a dot

--- jobs/people.rb
+++ jobs/people.rb
@@ -15,7 +15,7 @@ SCHEDULER.every '180m', first_in: 0 do |job|
   cal_file = Kernel.open("https://app.timetastic.co.uk/Feeds/OrganisationCalendar/#{key}")
   cals = Icalendar.parse(cal_file)
   cal  = cals.first
-  vacation_people_hash = cal.events.select{|e| (e.dtstart..e.dtend).cover? Icalendar::Values::Date.new(Date.today) }.map{|e| { value: e.summary.force_encoding("UTF-8").split(" ",2)[0] } }
+  vacation_people_hash = cal.events.select{|e| (e.dtstart...e.dtend).cover? Icalendar::Values::Date.new(Date.today) }.map{|e| { value: e.summary.force_encoding("UTF-8").split(" ",2)[0] } }
   vacation_people = vacation_people_hash.map(&:values).flatten
   cal_file.close
 
